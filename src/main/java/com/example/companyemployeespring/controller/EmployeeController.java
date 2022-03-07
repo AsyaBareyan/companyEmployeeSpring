ba20386763e87445c03eca0007dc71e5e69227c2
Asya <asyabareyan@gmail.com>
2022-03-08 02:18:43 +0400

    Company_Employee

--- src/main/java/com/example/companyemployeespring/controller/EmployeeController.java
+++ src/main/java/com/example/companyemployeespring/controller/EmployeeController.java
@@ -4,14 +4,19 @@ import com.example.companyemployeespring.entity.Company;
 import com.example.companyemployeespring.entity.Employee;
 import com.example.companyemployeespring.repository.CompanyRepository;
 import com.example.companyemployeespring.repository.EmployeeRepository;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Controller
@@ -20,6 +25,9 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 @Autowired
     CompanyRepository companyRepository;
+
+@Value("${companyemployee.upload.path}")
+public String imagePath;
 
 @GetMapping("/employees")
     public String employees(ModelMap map){
@@ -44,8 +52,19 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees/add")
-    public String addEmployee(@ModelAttribute Employee employee) {
+    public String addEmployee(@ModelAttribute Employee employee,@RequestParam("picture") MultipartFile uploadedFile) throws IOException {
+        if (!uploadedFile.isEmpty()) {
+            String fileName = System.currentTimeMillis() + "_" + uploadedFile.getOriginalFilename();
+            File newFile = new File(imagePath + fileName);
+            uploadedFile.transferTo(newFile);
+            employee.setPicUrl(fileName);
+        }
         employeeRepository.save(employee);
         return "redirect:/employees";
+    }
+    @GetMapping(value = "/getImage",produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImage(@RequestParam("picName") String picName) throws IOException {
+        InputStream inputStream=new FileInputStream(imagePath+picName);
+        return IOUtils.toByteArray(inputStream);
     }
 }
--- src/main/java/com/example/companyemployeespring/entity/Employee.java
+++ src/main/java/com/example/companyemployeespring/entity/Employee.java
@@ -25,5 +25,6 @@ public class Employee {
     private String position;
     @ManyToOne(fetch = FetchType.EAGER)
     private Company company;
+    private String picUrl;
 
 }
